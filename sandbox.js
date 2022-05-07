91bbf781a7ab37047e5e6311c7da7b52d27622d3
MSajedian <mohammadsajedian@gmail.com>
2022-05-07 14:49:39 +0100

    Objects & Arrays in TypeScript

--- sandbox.js
+++ sandbox.js
@@ -1,14 +1,62 @@
-var character = 'mario';
-var age = 30;
-var isBlackBelt = false;
+// TypeScript for JavaScript Programmers
+// https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html
+// JavaScript types
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures#javascript_types
+// typeof()
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
+// ******** String ********
+var character = "mario";
+character = "luigi";
+// impossible:
 // character = 20;
-character = 'luigi';
-// age = 'yoshi';
+// ******** Number ********
+var age = 30;
 age = 40;
-// isBlackBelt = 'yes';
+// impossible:
+// age = 'yoshi';
+// ******** Boolean ********
+var isBlackBelt = false;
 isBlackBelt = true;
+// impossible:
+// isBlackBelt = 'yes';
+// ******** Function ********
 var circ = function (diameter) {
     return diameter * Math.PI;
 };
 // console.log(circ('hello'));
 console.log(circ(7.5));
+// ******** Array ********
+var names = ["luigi", "mario", "yoshi"];
+names.push("toad");
+// impossible:
+// names.push(3);
+// names[1] = 3;
+var numbers = [10, 20, 30, 40];
+numbers.push(3);
+// numbers.push("toad");
+// numbers[1] = 3;
+var mixed = ['ken', 4, 'chun-li', 8, 9];
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 3;
+// ******** Object ********
+var ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
+};
+ninja.age = 40;
+ninja.name = 'ryu';
+// impossible:
+// ninja.age = '30';
+// ninja.skills = ['fighting', 'sneaking']
+ninja = {
+    name: 'yoshi',
+    belt: 'orange',
+    age: 40
+};
+// impossible:
+// ninja = {
+//   name: 'yoshi',
+//   belt: 'orange',
+// };
--- sandbox.ts
+++ sandbox.ts
@@ -1,19 +1,85 @@
-let character = 'mario';
-let age = 30;
-let isBlackBelt = false;
+// TypeScript for JavaScript Programmers
+// https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html
 
+// JavaScript types
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures#javascript_types
+
+// typeof()
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
+
+// ******** String ********
+let character = "mario";
+character = "luigi";
+
+// impossible:
 // character = 20;
-character = 'luigi';
 
-// age = 'yoshi';
+// ******** Number ********
+let age = 30;
+
 age = 40;
 
-// isBlackBelt = 'yes';
-isBlackBelt = true;
+// impossible:
+// age = 'yoshi';
 
+// ******** Boolean ********
+let isBlackBelt = false;
+
+isBlackBelt = true;
+// impossible:
+// isBlackBelt = 'yes';
+
+// ******** Function ********
 const circ = (diameter: number) => {
   return diameter * Math.PI;
 };
 
 // console.log(circ('hello'));
 console.log(circ(7.5));
+
+// ******** Array ********
+let names = ["luigi", "mario", "yoshi"];
+
+names.push("toad");
+// impossible:
+// names.push(3);
+// names[1] = 3;
+
+let numbers = [10, 20, 30, 40];
+
+numbers.push(3);
+// numbers.push("toad");
+// numbers[1] = 3;
+
+let mixed = ['ken', 4, 'chun-li', 8, 9];
+
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 3;
+
+// ******** Object ********
+let ninja = {
+  name: 'mario',
+  belt: 'black',
+  age: 30
+};
+
+ninja.age = 40;
+ninja.name = 'ryu';
+// impossible:
+// ninja.age = '30';
+// ninja.skills = ['fighting', 'sneaking']
+
+ninja = {
+  name: 'yoshi',
+  belt: 'orange',
+  age: 40,
+  // impossible:
+  // skills: ['running'],
+};
+
+// impossible:
+// ninja = {
+//   name: 'yoshi',
+//   belt: 'orange',
+// };
