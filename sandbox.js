1c591ad5ca9e331d9dbee90716a7284ed6497711
MSajedian <mohammadsajedian@gmail.com>
2022-05-09 12:15:48 +0100

    Dynamic (any) Types

--- sandbox.js
+++ sandbox.js
@@ -5,20 +5,21 @@
 // typeof()
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
 // ******** String ********
-var character = "mario";
-character = "luigi";
+var characterOne = "mario";
+characterOne = "luigi";
 // impossible:
-// character = 20;
+// characterOne = 20;
 // ******** Number ********
-var age = 30;
-age = 40;
+var ageOne = 30;
+ageOne = 40;
 // impossible:
-// age = 'yoshi';
+// ageOne = 'yoshi';
 // ******** Boolean ********
 var isBlackBelt = false;
 isBlackBelt = true;
 // impossible:
 // isBlackBelt = 'yes';
+// ****************************************************
 // ******** Function ********
 var circ = function (diameter) {
     return diameter * Math.PI;
@@ -35,28 +36,81 @@ var numbers = [10, 20, 30, 40];
 numbers.push(3);
 // numbers.push("toad");
 // numbers[1] = 3;
-var mixed = ['ken', 4, 'chun-li', 8, 9];
-mixed.push('ryu');
-mixed.push(10);
-mixed[0] = 3;
+var mixedOne = ["ken", 4, "chun-li", 8, 9];
+mixedOne.push("ryu");
+mixedOne.push(10);
+mixedOne[0] = 3;
 // ******** Object ********
-var ninja = {
-    name: 'mario',
-    belt: 'black',
+var ninjaOne = {
+    name: "mario",
+    belt: "black",
     age: 30
 };
-ninja.age = 40;
-ninja.name = 'ryu';
+ninjaOne.age = 40;
+ninjaOne.name = "ryu";
 // impossible:
-// ninja.age = '30';
-// ninja.skills = ['fighting', 'sneaking']
-ninja = {
-    name: 'yoshi',
-    belt: 'orange',
+// ninjaOne.age = '30';
+// ninjaOne.skills = ['fighting', 'sneaking']
+ninjaOne = {
+    name: "yoshi",
+    belt: "orange",
     age: 40
 };
 // impossible:
-// ninja = {
+// ninjaOne = {
 //   name: 'yoshi',
 //   belt: 'orange',
 // };
+// ****************************************************
+// **************** Explicit Types ****************
+var characterTwo = "mario";
+var ageTwo;
+var isLoggedIn;
+ageTwo = 30;
+// impossible:
+// age = 'luigi';
+isLoggedIn = true;
+// impossible:
+// isLoggedIn = 25;
+// ******** Arrays ********
+var ninjasOne = [];
+ninjasOne.push("ryu");
+ninjasOne.push("chun-li");
+console.log(ninjasOne);
+// impossible:
+// ninjasOne = [10, 23];
+// impossible: (not initilised with empty array)
+// let ninjas2: string[];
+// ninjas2.push("ryu");
+// ******** Union Types ********
+var mixedTwo = [];
+mixedTwo.push("hello");
+mixedTwo.push(false);
+mixedTwo.push(20);
+console.log(mixedTwo);
+var uid;
+// ******** Objects ********
+var ninjaTwo;
+ninjaTwo = { name: "yoshi", age: 30 };
+var ninjaThree;
+ninjaThree = { name: "ken", age: 20, beltColour: "black" };
+// ****************************************************
+// ******** Any ********
+var age = 25;
+age = true;
+console.log(age);
+age = 'hello';
+console.log(age);
+age = { name: 'luigi' };
+console.log(age);
+var mixed = [];
+mixed.push(5);
+mixed.push('mario');
+mixed.push(false);
+console.log(mixed);
+var ninja;
+ninja = { name: 'yoshi', age: 25 };
+console.log(ninja);
+ninja = { name: 25, age: 'yoshi' };
+console.log(ninja);
+// ****************************************************
