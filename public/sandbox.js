110158f60b6ca599865281ab4e21436806a5fad7
MSajedian <mohammadsajedian@gmail.com>
2022-05-10 12:39:36 +0100

    added tsconfig & Workflow

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,160 +1,121 @@
+"use strict";
 // TypeScript for JavaScript Programmers
 // https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html
-
 // JavaScript types
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures#javascript_types
-
 // typeof()
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
-
 // ******** String ********
 let characterOne = "mario";
 characterOne = "luigi";
-
 // impossible:
 // characterOne = 20;
-
 // ******** Number ********
 let ageOne = 30;
-
 ageOne = 40;
-
 // impossible:
 // ageOne = 'yoshi';
-
 // ******** Boolean ********
 let isBlackBelt = false;
-
 isBlackBelt = true;
 // impossible:
 // isBlackBelt = 'yes';
-
 // ****************************************************
 // ******** Function ********
-const circ = (diameter: number) => {
-  return diameter * Math.PI;
+const circ = (diameter) => {
+    return diameter * Math.PI;
 };
-
 // console.log(circ('hello'));
 console.log(circ(7.5));
-
 // ******** Array ********
 let names = ["luigi", "mario", "yoshi"];
-
 names.push("toad");
 // impossible:
 // names.push(3);
 // names[1] = 3;
-
 let numbers = [10, 20, 30, 40];
-
 numbers.push(3);
 // numbers.push("toad");
 // numbers[1] = 3;
-
 let mixedOne = ["ken", 4, "chun-li", 8, 9];
-
 mixedOne.push("ryu");
 mixedOne.push(10);
 mixedOne[0] = 3;
-
 // ******** Object ********
 let ninjaOne = {
-  name: "mario",
-  belt: "black",
-  age: 30,
+    name: "mario",
+    belt: "black",
+    age: 30,
 };
-
 ninjaOne.age = 40;
 ninjaOne.name = "ryu";
 // impossible:
 // ninjaOne.age = '30';
 // ninjaOne.skills = ['fighting', 'sneaking']
-
 ninjaOne = {
-  name: "yoshi",
-  belt: "orange",
-  age: 40,
-  // impossible:
-  // skills: ['running'],
+    name: "yoshi",
+    belt: "orange",
+    age: 40,
+    // impossible:
+    // skills: ['running'],
 };
-
 // impossible:
 // ninjaOne = {
 //   name: 'yoshi',
 //   belt: 'orange',
 // };
-
 // ****************************************************
 // **************** Explicit Types ****************
-
-let characterTwo: string = "mario";
-let ageTwo: number;
-let isLoggedIn: boolean;
-
+let characterTwo = "mario";
+let ageTwo;
+let isLoggedIn;
 ageTwo = 30;
 // impossible:
 // age = 'luigi';
-
 isLoggedIn = true;
 // impossible:
 // isLoggedIn = 25;
-
 // ******** Arrays ********
-let ninjasOne: string[] = [];
-
+let ninjasOne = [];
 ninjasOne.push("ryu");
 ninjasOne.push("chun-li");
 console.log(ninjasOne);
 // impossible:
 // ninjasOne = [10, 23];
-
 // impossible: (not initilised with empty array)
 // let ninjas2: string[];
 // ninjas2.push("ryu");
-
 // ******** Union Types ********
-let mixedTwo: (string | number | boolean)[] = [];
+let mixedTwo = [];
 mixedTwo.push("hello");
 mixedTwo.push(false);
 mixedTwo.push(20);
 console.log(mixedTwo);
-
-let uid: string | number;
-
+let uid;
 // ******** Objects ********
-let ninjaTwo: object;
+let ninjaTwo;
 ninjaTwo = { name: "yoshi", age: 30 };
-
-let ninjaThree: {
-  name: string;
-  age: number;
-  beltColour: string;
-};
+let ninjaThree;
 ninjaThree = { name: "ken", age: 20, beltColour: "black" };
 // ****************************************************
 // ******** Any ********
-let age: any = 25;
-
+let age = 25;
 age = true;
 console.log(age);
 age = 'hello';
 console.log(age);
 age = { name: 'luigi' };
 console.log(age);
-
-let mixed: any[] = [];
-
+let mixed = [];
 mixed.push(5);
 mixed.push('mario');
 mixed.push(false);
 console.log(mixed);
-
-let ninja: { name: any, age: any };
-
+let ninja;
 ninja = { name: 'yoshi', age: 25 };
 console.log(ninja);
-
 ninja = { name: 25, age: 'yoshi' };
 console.log(ninja);
+// ****************************************************
+console.log("tscongif.json  was created");
 // ****************************************************
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,116 +1,162 @@
 // TypeScript for JavaScript Programmers
 // https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html
+
 // JavaScript types
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures#javascript_types
+
 // typeof()
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
+
 // ******** String ********
-var characterOne = "mario";
+let characterOne = "mario";
 characterOne = "luigi";
+
 // impossible:
 // characterOne = 20;
+
 // ******** Number ********
-var ageOne = 30;
+let ageOne = 30;
+
 ageOne = 40;
+
 // impossible:
 // ageOne = 'yoshi';
+
 // ******** Boolean ********
-var isBlackBelt = false;
+let isBlackBelt = false;
+
 isBlackBelt = true;
 // impossible:
 // isBlackBelt = 'yes';
+
 // ****************************************************
 // ******** Function ********
-var circ = function (diameter) {
-    return diameter * Math.PI;
+const circ = (diameter: number) => {
+  return diameter * Math.PI;
 };
+
 // console.log(circ('hello'));
 console.log(circ(7.5));
+
 // ******** Array ********
-var names = ["luigi", "mario", "yoshi"];
+let names = ["luigi", "mario", "yoshi"];
+
 names.push("toad");
 // impossible:
 // names.push(3);
 // names[1] = 3;
-var numbers = [10, 20, 30, 40];
+
+let numbers = [10, 20, 30, 40];
+
 numbers.push(3);
 // numbers.push("toad");
 // numbers[1] = 3;
-var mixedOne = ["ken", 4, "chun-li", 8, 9];
+
+let mixedOne = ["ken", 4, "chun-li", 8, 9];
+
 mixedOne.push("ryu");
 mixedOne.push(10);
 mixedOne[0] = 3;
+
 // ******** Object ********
-var ninjaOne = {
-    name: "mario",
-    belt: "black",
-    age: 30
+let ninjaOne = {
+  name: "mario",
+  belt: "black",
+  age: 30,
 };
+
 ninjaOne.age = 40;
 ninjaOne.name = "ryu";
 // impossible:
 // ninjaOne.age = '30';
 // ninjaOne.skills = ['fighting', 'sneaking']
+
 ninjaOne = {
-    name: "yoshi",
-    belt: "orange",
-    age: 40
+  name: "yoshi",
+  belt: "orange",
+  age: 40,
+  // impossible:
+  // skills: ['running'],
 };
+
 // impossible:
 // ninjaOne = {
 //   name: 'yoshi',
 //   belt: 'orange',
 // };
+
 // ****************************************************
 // **************** Explicit Types ****************
-var characterTwo = "mario";
-var ageTwo;
-var isLoggedIn;
+
+let characterTwo: string = "mario";
+let ageTwo: number;
+let isLoggedIn: boolean;
+
 ageTwo = 30;
 // impossible:
 // age = 'luigi';
+
 isLoggedIn = true;
 // impossible:
 // isLoggedIn = 25;
+
 // ******** Arrays ********
-var ninjasOne = [];
+let ninjasOne: string[] = [];
+
 ninjasOne.push("ryu");
 ninjasOne.push("chun-li");
 console.log(ninjasOne);
 // impossible:
 // ninjasOne = [10, 23];
+
 // impossible: (not initilised with empty array)
 // let ninjas2: string[];
 // ninjas2.push("ryu");
+
 // ******** Union Types ********
-var mixedTwo = [];
+let mixedTwo: (string | number | boolean)[] = [];
 mixedTwo.push("hello");
 mixedTwo.push(false);
 mixedTwo.push(20);
 console.log(mixedTwo);
-var uid;
+
+let uid: string | number;
+
 // ******** Objects ********
-var ninjaTwo;
+let ninjaTwo: object;
 ninjaTwo = { name: "yoshi", age: 30 };
-var ninjaThree;
+
+let ninjaThree: {
+  name: string;
+  age: number;
+  beltColour: string;
+};
 ninjaThree = { name: "ken", age: 20, beltColour: "black" };
 // ****************************************************
 // ******** Any ********
-var age = 25;
+let age: any = 25;
+
 age = true;
 console.log(age);
 age = 'hello';
 console.log(age);
 age = { name: 'luigi' };
 console.log(age);
-var mixed = [];
+
+let mixed: any[] = [];
+
 mixed.push(5);
 mixed.push('mario');
 mixed.push(false);
 console.log(mixed);
-var ninja;
+
+let ninja: { name: any, age: any };
+
 ninja = { name: 'yoshi', age: 25 };
 console.log(ninja);
+
 ninja = { name: 25, age: 'yoshi' };
 console.log(ninja);
+// ****************************************************
+console.log("tscongif.json  was created");
 // ****************************************************
