9676864eee0f41028c2c736db4aa8cfc645e4308
MSajedian <mohammadsajedian@gmail.com>
2022-05-27 18:33:41 +0100

    added Classes

--- public/app.js
+++ public/app.js
@@ -1,20 +1,47 @@
 "use strict";
-const anchor = document.querySelector('a');
+// ****************************************************
+// ******** The DOM & Type Casting ********
+const anchor = document.querySelector("a");
 if (anchor) {
-    console.log(anchor.href);
+    // console.log(anchor.href);
 }
-console.log(anchor.href);
+// console.log(anchor.href);
 //const form = document.querySelector('form')!;
-const form = document.querySelector('.new-item-form');
-console.log(form.children);
-// inputs
-const type = document.querySelector('#type');
-const tofrom = document.querySelector('#tofrom');
-const details = document.querySelector('#details');
-const amount = document.querySelector('#amount');
-form.addEventListener('submit', (e) => {
+const form = document.querySelector(".new-item-form");
+// console.log(form.children);
+// **** Inputs ****
+const type = document.querySelector("#type");
+const tofrom = document.querySelector("#tofrom");
+const details = document.querySelector("#details");
+const amount = document.querySelector("#amount");
+form.addEventListener("submit", (e) => {
     e.preventDefault();
     console.log(type.value, tofrom.value, details.value, 
     // amount.value
     amount.valueAsNumber);
 });
+// ****************************************************
+// ******** Classes ********
+class Invoice {
+    constructor(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes £${this.amount} for ${this.details}`;
+    }
+}
+const invOne = new Invoice("mario", "work on the mario website", 250);
+const invTwo = new Invoice("luigi", "work on the luigi website", 300);
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+// Impossible:
+// invoices.push({ "client": "client1", "details": "details1", "amount": 1 });
+// Impossible:
+// invoices.push({ name: 'shaun' });
+console.log('invoices:', invoices);
+console.log('invOne.format():', invOne.format());
+// ****************************************************
+// ********  ********
