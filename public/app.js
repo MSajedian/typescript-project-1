dfaabcff08ba728cdacbaf9c5639eefd01e133af
MSajedian <mohammadsajedian@gmail.com>
2022-06-14 02:48:40 +0100

    Tuples

--- public/app.js
+++ public/app.js
@@ -116,12 +116,14 @@ const ul = document.querySelector("ul");
 const list = new ListTemplate(ul);
 form.addEventListener("submit", (e) => {
     e.preventDefault();
+    let values;
+    values = [tofrom.value, details.value, amount.valueAsNumber];
     let doc;
-    if (type.value === "invoice") {
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    if (type.value === 'invoice') {
+        doc = new Invoice(...values);
     }
     else {
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Payment(...values);
     }
     list.render(doc, type.value, "end");
 });
@@ -173,3 +175,15 @@ const docSix = {
 };
 console.log("docFive:", docFive);
 console.log("docSix:", docSix);
+// ****************************************************
+// ******** Tuples ********
+let arr = ['ryu', 25, true];
+arr[0] = false;
+arr[1] = 'yoshi';
+arr = [30, false, 'yoshi'];
+let tup = ['ryu', 25, true];
+// tup[0] = false;
+tup[0] = 'ken';
+let student;
+//student = [23564, 'chun-li'];
+student = ['chun-li', 23564];
